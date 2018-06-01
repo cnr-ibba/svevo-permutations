a43c5507a5ce36a2ffb40b2712f3ba62081699ab
Paolo Cozzi <bunop@libero.it>
2018-06-01 16:27:43 +0200

    Adding FST torque job

---
+++ job_fst.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+#PBS -d /storage/ormanbekovad/svevo_permutations
+#PBS -l nodes=1:ppn=64
+#PBS -N FST-permutations
+#PBS -q general
+#PBS -j oe
+
+##########################################
+#                                        #
+#   Output some useful job information.  #
+#                                        #
+##########################################
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo ------------------------------------------------------
+
+# Starting job
+echo "job $PBS_JOBNAME started: `whoami` `hostname` `pwd` `date`."
+
+##########################################
+#                                        #
+#   Environment settings                 #
+#                                        #
+##########################################
+
+# Better to run this. Job will fail with unbounded (empty) variables, or a single error in the script
+set -e
+set -u
+set -o pipefail
+
+# activate conda environment
+source activate R-3.4
+
+##########################################
+#                                        #
+#   Calling R script                     #
+#                                        #
+##########################################
+
+
+# executing Rscript
+Rscript --slave --vanilla Fst_permutation_unibo_1M.R
+
+##########################################
+#                                        #
+#   Ending job                           #
+#                                        #
+##########################################
+
+# finishing job
+echo "job $PBS_JOBNAME finished `whoami` `hostname` `pwd` `date`."
